cdc804b3d6416b93e51f6d4e4ed914eeb1f95c9b
Mugym20 <130445291+Mugym20@users.noreply.github.com>
2023-08-12 11:04:14 +0700

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/uas_akb_if2_10120049/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/uas_akb_if2_10120049/ExampleInstrumentedTest.java
@@ -1,5 +1,9 @@
 package com.example.uas_akb_if2_10120049;
 
+// Nama: Mochammad Gymnastiar
+// Nim: 10120049
+// Kelas: IF2
+
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
