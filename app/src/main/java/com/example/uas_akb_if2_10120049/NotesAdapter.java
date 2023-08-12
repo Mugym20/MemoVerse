4e32bd7d9b39724c0ed1ed9221c19c5a1f39164f
Mugym20 <mohhamadgymnastian@gmail.com>
2023-08-12 11:01:30 +0700

    MemoVerse

---
+++ app/src/main/java/com/example/uas_akb_if2_10120049/NotesAdapter.java
@@ -0,0 +1,73 @@
+package com.example.uas_akb_if2_10120049;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import org.w3c.dom.Document;
+
+import java.util.List;
+
+public class NotesAdapter extends FirestoreRecyclerAdapter<Note, NotesAdapter.NoteHolder> {
+    private onItemClickListener listener;
+    public NotesAdapter(@NonNull FirestoreRecyclerOptions<Note> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull NoteHolder holder, int position, @NonNull Note model) {
+        holder.noteTitle.setText(model.getTitle());
+        holder.noteDescription.setText(model.getDescription());
+        holder.noteCategory.setText(model.getCategory());
+    }
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.notes_slider_layout, parent, false);
+        return new NoteHolder(v);
+    }
+
+    public void deleteItem(int position) {
+        getSnapshots().getSnapshot(position).getReference().delete();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder{
+        TextView noteTitle, noteCategory, noteDescription;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            noteTitle = itemView.findViewById(R.id.note_title);
+            noteCategory = itemView.findViewById(R.id.note_category);
+            noteDescription = itemView.findViewById(R.id.note_desc);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    if (position != RecyclerView.NO_POSITION && listener != null) {
+                        listener.onItemClick(getSnapshots().getSnapshot(position), position);
+                    }
+                }
+            });
+        }
+    }
+
+    public interface onItemClickListener {
+        void onItemClick(DocumentSnapshot documentSnapshot, int position);
+    }
+
+    public void setOnItemClickListener(onItemClickListener listener) {
+        this.listener = listener;
+    }
+}
